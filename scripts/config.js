6fc91946566e110ce8c2670ab71e1219c50e3693
3poitn14guy <sunshine.farm.nc@gmail.com>
2017-07-18 21:19:51 -0400

    added authorization files
    -added files to work with user table

---
+++ scripts/config.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const config = {
+  apiOrigins: {
+    // think i need to use localhost for development
+    // development: 'http://tic-tac-toe.wdibos.com',
+    //  below might stay viable for authorzation on this project
+    production: 'https://aqueous-atoll-85096.herokuapp.com/'
+  }
+}
+
+module.exports = config
